183249f0469ffbb099aee25934bc03d3dd5149b6
bodroghykristof <bodroghykristof@gmail.com>
2020-07-28 12:40:15 +0200

    Create basics of main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
+socketio = SocketIO(app)
+
+
+if __name__ == '__main__':
+    socketio.run(app)
