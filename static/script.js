3ed31fe8be0521b65df9e781c732b6d9644d5276
bodroghykristof <bodroghykristof@gmail.com>
2020-07-28 14:30:14 +0200

    Implement sending and receiving messages

--- static/script.js
+++ static/script.js
@@ -1,6 +1,24 @@
 const socket = io.connect('http://127.0.0.1:5000/');
-socket.addEventListener('connect', logConnectionOnServer)
+const sendButton = document.querySelector('#send-button');
+
+socket.addEventListener('connect', logConnectionOnServer);
+socket.addEventListener('message', addMessageToChatBoard);
+
+sendButton.addEventListener('click', sendMessageToServer);
 
 function logConnectionOnServer() {
     socket.send('User has connected')
+}
+
+function addMessageToChatBoard(message) {
+    const chatBoard = document.querySelector('.chat-board');
+    let newMessage = document.createElement('p');
+    newMessage.innerHTML = message;
+    chatBoard.appendChild(newMessage);
+}
+
+function sendMessageToServer() {
+    let newMessage = document.querySelector('#message').value;
+    document.querySelector('#message').value =  '';
+    socket.send(newMessage);
 }
