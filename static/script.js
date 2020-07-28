de2d2875c2b4ddb49846911d813dd4b1dc2beab0
bodroghykristof <bodroghykristof@gmail.com>
2020-07-28 13:31:18 +0200

    Implement logging connected users on server side

--- static/script.js
+++ static/script.js
@@ -1,1 +1,6 @@
 const socket = io.connect('http://127.0.0.1:5000/');
+socket.addEventListener('connect', logConnectionOnServer)
+
+function logConnectionOnServer() {
+    socket.send('User has connected')
+}
